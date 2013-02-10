6ced90df9eae0129a6dfbffb08f597a9ef783dd9
Diony Rosa <dhrosa@gmail.com>
2013-02-09 19:15:26 -0500

    Removed unneccessary svn to git scripts, as 6.005 now uses git.

--- example/gen-svn-pull.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-echo "adding all *.java files from svn to SVN-FILES"
-find src -name "*.java" -print >> SVN-FILES
-echo "removing repeated lines in SVN-FILES"
-sort -u SVN-FILES -o SVN-FILES
-echo "remember to remove any nonexistent files from SVN-FILES manually!"
--- example/gen-svn-push.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-echo "adding all *.java files in git to SVN-FILES"
-find src -name "*.java" -print >> SVN-FILES
-echo "removing repeated lines in SVN-FILES"
-sort -u SVN-FILES -o SVN-FILES
-echo "remember to remove any deleted files from SVN-FILES manually!"
